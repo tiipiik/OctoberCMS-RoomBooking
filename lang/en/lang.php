b08899f9ed9e944bd3994a6a0fb821be1d530cca
tiipiik <yohann.decharraud@gmail.com>
2014-08-05 18:45:20 +0200

    Made backend translatable

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,67 @@
 <?php
 
 return [
-    'bookingform' => [
+    'booking' => [
+        'amount'=>'Amount',
+        'currency'=>'Currency',
+        'validated'=>'Validated',
+        'registered'=>'Registered',
+        
         'backend' => [
             'new' => 'New Booking',
+            'room_id_label'=>'Room',
+            'visitor'=>[
+                'tab_title'=>'Visitor',
+                'full_name'=>'Full name',
+                'email'=>'E-mail',
+                'phone'=>'Phone number',
+                'persons'=>'Persons',
+                'rooms'=>'Rooms',
+                'comment'=>'Comment',
+            ],
+            'stay'=>[
+                'tab_title'=>'Visitor stay',
+                'checkin'=>'Check in',
+                'checkout'=>'Check out',
+                'nights'=>'Total nights',
+            ],
+            'pay_plan'=>[
+                'tab_title'=>'Pay plan',
+                'options_title'=>'Pay plan',
+            ],
+            'columns'=>[
+                'id'=>'ID',
+                'room_name'=>'Room name or number',
+                'customer_name'=>'Full name',
+                'customer_email'=>'E-mail',
+                'customer_phone'=>'Phone',
+                'persons'=>'Persons',
+                'rooms'=>'Rooms',
+                'checkin'=>'Check In',
+                'checkout'=>'Check Out',
+                'total_days'=>'Total days',
+                'total_nights'=>'Total nights',
+                'pay_plan'=>'Pay plan',
+                'comment'=>'Comment',
+            ]
         ],
     ],
     'room' => [
-        'backend'=> [
+        'backend' => [
             'new'=>'New Room',
+            'name'=>'Room name or Number',
+            'name_placeholder'=>'New room name or number',
+            'slug'=>'Slug',
+            'slug_placeholder'=>'new-room-name-or-number',
+            'description_tab_title'=>'Full description',
+            'manage_tab_title'=>'Manage',
+            'excerpt'=>'Excerpt',
+            'max_persons'=>'Max number of persons in the room ',
+            'featured_images'=>'Featured images',
+            'columns'=> [
+                'name'=>'Name or Number',
+                'available'=>'Available',
+            ]
         ],
     ]
 ];
